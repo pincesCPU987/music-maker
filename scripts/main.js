ee718af551c21b2a2ba4125a4672008b775ff4b5
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2022-10-17 10:56:36 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
+const url = "https://upload.wikimedia.org/wikipedia/commons/9/90/De-Wolfgang_Amadeus_Mozart.ogg"; // use any valid audio file if this disappears over time...
 
+const btn = document.querySelector("button");
+btn.addEventListener("click", async () => {
+  await Tone.start();
+  const player = new Tone.Player({
+    url,
+    loop: true,
+    autostart: false,
+  });
+  await Tone.loaded();
+  const pitchShift = new Tone
+    .PitchShift({pitch: -8})
+    .toDestination()
+  ;
+  player.connect(pitchShift);
+  player.start();
+});
